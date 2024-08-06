288f672f8fa65710312da4ba01c16d601feb5cb1
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 14:28:02 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x/y);
+
+    }
+}
